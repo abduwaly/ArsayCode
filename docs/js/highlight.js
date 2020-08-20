b04e7fba1c274762730b4398fecb1e5d235935f9
Abduwaly <arsay@foxmail.com>
2020-08-20 20:02:51 +0800

    Update highlight.js

--- docs/js/highlight.js
+++ docs/js/highlight.js
@@ -1,4 +1,5 @@
 // adding this to highlight inline code block
+// adding normal text for testing
 $(document).ready(function() {
     $('p code').each(function(i, inline) {
         hljs.highlightBlock(inline);
